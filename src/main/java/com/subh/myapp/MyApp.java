1327168bc0d8b69aa3e996921f5f5cea8772f21b
Chaudhary, Subash <su.bash.upgrad@gmail.com>
2022-07-03 23:35:08 +0530

    Initial Commit

---
+++ src/main/java/com/subh/myapp/MyApp.java
@@ -0,0 +1,19 @@
+package com.subh.myapp
+
+public class MyApp{
+
+    
+    private final String message = "Hello World!!!";
+
+
+    public MyApp() {}
+
+    public static void main(String[] args) {
+        System.out.println(new MyApp().getMessage())
+    }
+
+
+    private final String getMessage() {
+        return message;
+    }
+}
